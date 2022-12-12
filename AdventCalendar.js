0833893c2b53f4928ec132ec6a90e294a14bd3fb
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2022-12-12 10:13:41 +0100

    change display

--- AdventCalendar.js
+++ AdventCalendar.js
@@ -1,13 +1,12 @@
 // Création des 24 links vers les pages
 
-let liste = [1, 9, 11, 14, 3, 19, 15, 6, 
-            21, 24, 17, 8, 23, 5, 13, 18, 
-            20, 4, 10, 22, 12, 7, 2, 16];
+let liste = [1, 9, 11, 14, 3, 19, 15, 6,
+    21, 24, 17, 8, 23, 5, 13, 18,
+    20, 4, 10, 22, 12, 7, 2, 16];
 
 const conteneurDiv = document.querySelector('#conteneur');
 
-for(var i= 0; i < liste.length; i++)
-{
+for (var i = 0; i < liste.length; i++) {
     let newDiv = document.createElement('div');
 
     let desiredLink = `recettes/jour_${liste[i]}.html`;
